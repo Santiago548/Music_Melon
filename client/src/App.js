5574e1b57bfc39c3d692ceb864b4ae67756003c1
Santiago548 <dominicbarajas548@gmail.com>
2021-08-12 13:49:04 -0600

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Login from './Login';
+
+function App() {
+  return (
+    <div className="App">
+     <Login />
+    </div>
+  );
+}
+
+export default App;
