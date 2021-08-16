ea7190ef95886c908fd70f5d8b04286b94137111
Santiago548 <dominicbarajas548@gmail.com>
2021-08-16 14:09:18 -0600

    add spotify web api node npm

--- client/src/Dashbord.js
+++ client/src/Dashbord.js
@@ -1,14 +1,22 @@
 import React from 'react'
+import { useState } from 'react'
 import useAuth from "./useAuth"
 import { Container, Form } from "react-bootstrap"
 
 export default function Dashbord({ code }) {
     const accessToken = useAuth(code)
-    return <Container>
+    const [ search, setSearch ] = useState("")
+    return <Container className="d-flex flex-column py-2" style={{
+        height: "100vh"}}>
             <Form.Control 
             type="Search" 
             placeholder="Search songs/ Artist" 
             value={search} 
             onChange={e => setSearch(e.target.value)}
+            />
+            <div ClassName="flex-grow-1 my-2" style={{ overflowY: "auto"}}>
+                Songs
+            </div>
+            <div>Bottom</div>
         </Container>
 }
